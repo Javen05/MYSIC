e9215d160a9f58f27f49627aca201803fe3a69f1
Javen <107395637+Javen05@users.noreply.github.com>
2023-02-26 13:31:22 +0800

    Initial commit

---
+++ MYSIC_final/app/src/main/java/sg/edu/tp/mysicmysic/PlaylistActivity.java
@@ -0,0 +1,58 @@
+package sg.edu.tp.mysicmysic;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import sg.edu.tp.mysicmysic.SongDatabase.SongCollection;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+
+        MyAdapter myAdapter = new MyAdapter(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                LinearLayoutManager.VERTICAL));
+
+        recyclerView.setAdapter(myAdapter);
+
+        //set song count
+        TextView songCount = findViewById(R.id.songCount);
+        songCount.setText(new SongCollection().getArrayLength() + " " + "Songs in List");
+
+
+        //backspace
+        ImageButton btnBack;
+
+        btnBack = (ImageButton)findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+}
+
+
+
+
